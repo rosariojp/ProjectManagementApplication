1b6f6f39bc0b67b4e008af390cd7120a8bec7ab5
rosariojp <rosario.jeipz@gmail.com>
2020-09-04 19:33:50 +0800

    Add functionality to Projects Controller to navigate list-projects.html

--- project-management/src/main/java/com/jeipz/pma/controllers/ProjectController.java
+++ project-management/src/main/java/com/jeipz/pma/controllers/ProjectController.java
@@ -1,5 +1,7 @@
 package com.jeipz.pma.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +18,13 @@ public class ProjectController {
 
 	@Autowired
 	ProjectRepository proRepo;
+	
+	@GetMapping
+	public String displayProjects(Model model) {
+		List<Project> projects = proRepo.findAll();
+		model.addAttribute("projectsList", projects);
+		return "projects/list-projects";
+	}
 	
 	@GetMapping("/new")
 	public String displayProjectForm(Model model) {
