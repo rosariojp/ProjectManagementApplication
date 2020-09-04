e412305715b916a28295f21ff5fb2e3945d88c13
rosariojp <rosario.jeipz@gmail.com>
2020-09-04 18:00:53 +0800

    Overrides findAll method from CrudRepository to return specific type of data for Employee and Project repository

--- project-management/src/main/java/com/jeipz/pma/dao/EmployeeRepository.java
+++ project-management/src/main/java/com/jeipz/pma/dao/EmployeeRepository.java
@@ -1,9 +1,14 @@
 package com.jeipz.pma.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.jeipz.pma.entities.Employee;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
+	@Override
+	public List<Employee> findAll();
+	
 }
--- project-management/src/main/java/com/jeipz/pma/dao/ProjectRepository.java
+++ project-management/src/main/java/com/jeipz/pma/dao/ProjectRepository.java
@@ -1,9 +1,14 @@
 package com.jeipz.pma.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.jeipz.pma.entities.Project;
 
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 
+	@Override
+	public List<Project> findAll();
+	
 }
