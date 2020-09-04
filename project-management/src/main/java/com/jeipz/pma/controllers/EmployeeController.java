a5129fff1d0640eaba72290cc4588eb5322e76d1
rosariojp <rosario.jeipz@gmail.com>
2020-09-04 17:24:04 +0800

    Implemented new Employee Controller and new-employee.html

---
+++ project-management/src/main/java/com/jeipz/pma/controllers/EmployeeController.java
@@ -0,0 +1,33 @@
+package com.jeipz.pma.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jeipz.pma.dao.EmployeeRepository;
+import com.jeipz.pma.entities.Employee;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	@Autowired
+	EmployeeRepository empRepo;
+	
+	@GetMapping("/new")
+	public String displayEmployeeForm(Model model) {
+		Employee anEmployee = new Employee();
+		model.addAttribute("employee", anEmployee);
+		return "new-employee";
+	}
+	
+	@PostMapping("/save")
+	public String createEmployee(Employee employee, Model model) {
+		empRepo.save(employee);
+		return "redirect:/employees/new";
+	}
+	
+}
