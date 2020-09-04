f4d00552fd66ebcbcc62a8e2a259101d26eba2f7
rosariojp <rosario.jeipz@gmail.com>
2020-09-04 16:25:30 +0800

    Added save functionality at Project controller

--- project-management/src/main/java/com/jeipz/pma/controllers/ProjectController.java
+++ project-management/src/main/java/com/jeipz/pma/controllers/ProjectController.java
@@ -1,17 +1,22 @@
 package com.jeipz.pma.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.jeipz.pma.dao.ProjectRepository;
 import com.jeipz.pma.entities.Project;
 
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
 
+	@Autowired
+	ProjectRepository proRepo;
+	
 	@GetMapping("/new")
 	public String displayProjectForm(Model model) {
 		Project aProject = new Project();
@@ -21,8 +26,8 @@ public class ProjectController {
 	
 	@PostMapping("/save")
 	public String createProject(Project project, Model model) {
-		// Should handle saving to the database
-		return "";
+		proRepo.save(project);
+		return "redirect:/projects/new";
 	}
 	
 }
