aeca72f64398cec9a898a257ef55edf146c1e4d3
rosariojp <rosario.jeipz@gmail.com>
2020-09-07 18:14:29 +0800

    Decode JSON String and populate it to the pieChart

--- project-management/src/main/resources/static/js/myChart.js
+++ project-management/src/main/resources/static/js/myChart.js
@@ -1,14 +1,38 @@
+var chartDataStr = decodeHtml(chartData);
+var chartJSONArray = JSON.parse(chartDataStr);
+
+var arrayLength = chartJSONArray.length;
+var numericData = [];
+var labelData = [];
+
+for (var i = 0; i < arrayLength; i++) {
+	numericData[i] = chartJSONArray[i].value;
+	labelData[i] = chartJSONArray[i].label;
+}
+
 // For a Pie Chart
 new Chart(document.getElementById("myPieChart"), {
 	type : 'pie',
 	data : {
-		labels : [ 'January', 'February', 'March' ],
+		labels : labelData,
 		datasets : [ {
 			label : 'My First dataset',
 			backgroundColor : [ "#3e95cd", "#8e5ea2", "#3cba9f" ],
-			borderColor : 'rgb(255, 99, 132)',
-			data : [ 5, 10, 5 ]
+			data : numericData
 		} ]
 	},
-	options : {}
+	options : {
+		title: {
+			display: true,
+			text: 'Project Statuses'
+		}
+	}
 });
+
+// "[{"value": 1, "label": "COMPLETED"},{"value": 2, "label":
+// "INPROGRESS"},{"value": 1, "label": "NOTSTARTED"}]"
+function decodeHtml(html) {
+	var txt = document.createElement("textarea");
+	txt.innerHTML = html;
+	return txt.value;
+}
