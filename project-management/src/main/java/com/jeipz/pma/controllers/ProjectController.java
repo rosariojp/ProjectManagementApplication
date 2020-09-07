788f645dd3edd94fcf63f2175f8351d37198c0de
rosariojp <rosario.jeipz@gmail.com>
2020-09-07 14:14:54 +0800

    Implemented handling of employee entity at project controller

--- project-management/src/main/java/com/jeipz/pma/controllers/ProjectController.java
+++ project-management/src/main/java/com/jeipz/pma/controllers/ProjectController.java
@@ -8,8 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.jeipz.pma.dao.EmployeeRepository;
 import com.jeipz.pma.dao.ProjectRepository;
+import com.jeipz.pma.entities.Employee;
 import com.jeipz.pma.entities.Project;
 
 @Controller
@@ -18,6 +21,9 @@ public class ProjectController {
 
 	@Autowired
 	ProjectRepository proRepo;
+	
+	@Autowired
+	EmployeeRepository empRepo;
 	
 	@GetMapping
 	public String displayProjects(Model model) {
@@ -29,13 +35,22 @@ public class ProjectController {
 	@GetMapping("/new")
 	public String displayProjectForm(Model model) {
 		Project aProject = new Project();
+		List<Employee> employees = empRepo.findAll();
 		model.addAttribute("project", aProject);
+		model.addAttribute("allEmployees", employees);
 		return "projects/new-project";
 	}
 	
 	@PostMapping("/save")
-	public String createProject(Project project, Model model) {
+	public String createProject(Project project, @RequestParam List<Long> employees, Model model) {
 		proRepo.save(project);
+		
+		Iterable<Employee> chosenEmployees = empRepo.findAllById(employees);
+		for (Employee employee : chosenEmployees) {
+			employee.setProject(project);
+			empRepo.save(employee);
+		}
+		
 		return "redirect:/projects/new";
 	}
 	
