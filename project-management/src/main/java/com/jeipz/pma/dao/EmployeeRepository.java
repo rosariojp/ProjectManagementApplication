db4f0c5ae6bd650b281568f564846214efea7c6e
rosariojp <rosario.jeipz@gmail.com>
2020-09-07 17:42:13 +0800

    Implemented new method to get employee project count

--- project-management/src/main/java/com/jeipz/pma/dao/EmployeeRepository.java
+++ project-management/src/main/java/com/jeipz/pma/dao/EmployeeRepository.java
@@ -2,13 +2,20 @@ package com.jeipz.pma.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import com.jeipz.pma.dto.EmployeeProject;
 import com.jeipz.pma.entities.Employee;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
 	@Override
 	public List<Employee> findAll();
-	
+
+	@Query(nativeQuery = true, value = "SELECT e.first_name AS firstName, e.last_name AS lastName, COUNT(pe.employee_id) As projectCount " + 
+			"FROM employee e LEFT JOIN project_employee pe ON pe.employee_id = e.employee_id " + 
+			"GROUP BY e.first_name, e.last_name ORDER BY 3 DESC")
+	public List<EmployeeProject> getEmployeeProjects();
+
 }
