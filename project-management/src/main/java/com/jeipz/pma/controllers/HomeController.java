6f2542fb11f7b13bfd1d37ae926dfaabc04c0cc6
rosariojp <rosario.jeipz@gmail.com>
2020-09-07 17:45:30 +0800

    Updated displayHomePage to use custom query result

--- project-management/src/main/java/com/jeipz/pma/controllers/HomeController.java
+++ project-management/src/main/java/com/jeipz/pma/controllers/HomeController.java
@@ -1,15 +1,20 @@
 package com.jeipz.pma.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jeipz.pma.dao.EmployeeRepository;
 import com.jeipz.pma.dao.ProjectRepository;
-import com.jeipz.pma.entities.Employee;
+import com.jeipz.pma.dto.ChartData;
+import com.jeipz.pma.dto.EmployeeProject;
 import com.jeipz.pma.entities.Project;
 
 @Controller
@@ -22,11 +27,23 @@ public class HomeController {
 	EmployeeRepository empRepo;
 
 	@GetMapping("/")
-	public String displayHomePage(Model model) {
+	public String displayHomePage(Model model) throws JsonProcessingException {
+		
+		Map<String, Object> map = new HashMap<>();
+		
 		List<Project> projects = proRepo.findAll();
 		model.addAttribute("projectsList", projects);
-		List<Employee> employees = empRepo.findAll();
-		model.addAttribute("employeesList", employees);
+		
+		List<ChartData> projectData = proRepo.getProjectStatus();
+
+		// Convert project Data to jason structure for use in javascript
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonString = objectMapper.writeValueAsString(projectData);
+		
+		model.addAttribute("projectStatusCount", jsonString);
+		
+		List<EmployeeProject> employeesProjectCount = empRepo.getEmployeeProjects();
+		model.addAttribute("employeesListProjectsCount", employeesProjectCount);
 		return "main/home";
 	}
 
