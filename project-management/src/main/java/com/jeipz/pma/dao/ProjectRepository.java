9971b4b0199295b3b4a0ba6a5c47163ae08f8395
rosariojp <rosario.jeipz@gmail.com>
2020-09-07 17:43:07 +0800

    Implemented new method to get project status

--- project-management/src/main/java/com/jeipz/pma/dao/ProjectRepository.java
+++ project-management/src/main/java/com/jeipz/pma/dao/ProjectRepository.java
@@ -2,13 +2,20 @@ package com.jeipz.pma.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import com.jeipz.pma.dto.ChartData;
 import com.jeipz.pma.entities.Project;
 
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 
 	@Override
 	public List<Project> findAll();
+	
+	@Query(nativeQuery = true, value = "SELECT stage AS label, COUNT(*) AS value " + 
+			"FROM project " + 
+			"GROUP BY stage")
+	public List<ChartData> getProjectStatus(); 
 	
 }
