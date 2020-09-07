04159279fe74c8b7795cf9f8e0793aeb6acbb279
rosariojp <rosario.jeipz@gmail.com>
2020-09-07 16:38:11 +0800

    Created JS file for creating a pie chart

---
+++ project-management/src/main/resources/static/js/myChart.js
@@ -0,0 +1,14 @@
+// For a Pie Chart
+new Chart(document.getElementById("myPieChart"), {
+	type : 'pie',
+	data : {
+		labels : [ 'January', 'February', 'March' ],
+		datasets : [ {
+			label : 'My First dataset',
+			backgroundColor : [ "#3e95cd", "#8e5ea2", "#3cba9f" ],
+			borderColor : 'rgb(255, 99, 132)',
+			data : [ 5, 10, 5 ]
+		} ]
+	},
+	options : {}
+});
