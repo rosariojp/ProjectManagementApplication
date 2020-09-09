ea7ae213f672c77a15ca0fed0673d0b997fbd44c
rosariojp <rosario.jeipz@gmail.com>
2020-09-09 17:20:50 +0800

    Rename package

--- project-management/src/test/java/com/jeipz/pma/dao/ProjectRepositoryIntegrationTest.java
+++ project-management/src/test/java/com/jeipz/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -1,26 +1,24 @@
-package com.jeipz.dao;
+package com.jeipz.pma.dao;
 
 import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.jeipz.pma.ProjectManagementApplication;
-import com.jeipz.pma.dao.ProjectRepository;
 import com.jeipz.pma.entities.Project;
 
-@ContextConfiguration(classes = ProjectManagementApplication.class)
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@DataJpaTest
-@SqlGroup({ @Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = { "classpath:schema.sql", "classpath:data.sql" }), 
-		    @Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:drop.sql" ) })
+@SqlGroup({ 
+	@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = { "classpath:schema.sql", "classpath:data.sql" }), 
+	@Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:drop.sql" )
+})
 public class ProjectRepositoryIntegrationTest {
 
 	@Autowired
