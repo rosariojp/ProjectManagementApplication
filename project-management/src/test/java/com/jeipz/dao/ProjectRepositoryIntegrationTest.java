dbb4a84e513a44a130f87c75932ea6e456847b45
rosariojp <rosario.jeipz@gmail.com>
2020-09-09 17:08:47 +0800

    Created a test for database save functionality

---
+++ project-management/src/test/java/com/jeipz/dao/ProjectRepositoryIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.jeipz.dao;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.jeipz.pma.ProjectManagementApplication;
+import com.jeipz.pma.dao.ProjectRepository;
+import com.jeipz.pma.entities.Project;
+
+@ContextConfiguration(classes = ProjectManagementApplication.class)
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@SqlGroup({ @Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD, scripts = { "classpath:schema.sql", "classpath:data.sql" }), 
+		    @Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:drop.sql" ) })
+public class ProjectRepositoryIntegrationTest {
+
+	@Autowired
+	ProjectRepository proRepo;
+	
+	@Test
+	public void ifNewProjectSaved_thenSuccess() {
+		Project newProject = new Project("New Test Project", "COMPLETE", "Test Description");
+		proRepo.save(newProject);
+		
+		assertEquals(5, proRepo.findAll().size());
+	}
+	
+}
