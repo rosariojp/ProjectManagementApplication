8cc4322c50f7db98988699813e890512dddff75f
rosariojp <rosario.jeipz@gmail.com>
2020-09-14 14:26:12 +0800

    Added authorization configuration

--- project-management/src/main/java/com/jeipz/pma/security/SecurityConfiguration.java
+++ project-management/src/main/java/com/jeipz/pma/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.jeipz.pma.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -21,13 +22,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.roles("USER")
 			.and()
 			.withUser("user")
-				.password("password")
-				.roles("USER");
+				.password("pass2")
+				.roles("USER")
+			.and()
+			.withUser("manageruser")
+				.password("pass3")
+				.roles("ADMIN");
+		
 	}
 	
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/projects/new").hasRole("ADMIN")
+			.antMatchers("/employees/new").hasRole("ADMIN")
+			.antMatchers("/").authenticated().and().formLogin();
 	}
 	
 }
