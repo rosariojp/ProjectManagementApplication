de744d94b28f0eabead189016b195e42f860d876
rosariojp <rosario.jeipz@gmail.com>
2020-09-14 15:43:24 +0800

    Created new Entity, User Account

---
+++ project-management/src/main/java/com/jeipz/pma/entities/UserAccount.java
@@ -0,0 +1,89 @@
+package com.jeipz.pma.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_accounts")
+public class UserAccount {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_accounts_seq")
+	@SequenceGenerator(name = "user_accounts_seq", sequenceName = "user_accounts_seq", allocationSize = 1)
+	@Column(name = "user_id")
+	private long userId;
+
+	@Column(name = "username")
+	private String userName;
+	
+	private String email;
+	private String password;
+	private String role;
+	private boolean enabled = true;
+
+	public UserAccount() {
+
+	}
+
+	public UserAccount(String username, String email, String password, String role, boolean enabled) {
+		this.userName = username;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+		this.enabled = enabled;
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return userName;
+	}
+
+	public void setUsername(String username) {
+		this.userName = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+}
