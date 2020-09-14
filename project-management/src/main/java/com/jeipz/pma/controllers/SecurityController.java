1c52a5cf55ae8eea24f16cef6d379e7a250e803e
rosariojp <rosario.jeipz@gmail.com>
2020-09-14 16:48:30 +0800

    Added register page and new controller for handling the registration

---
+++ project-management/src/main/java/com/jeipz/pma/controllers/SecurityController.java
@@ -0,0 +1,36 @@
+package com.jeipz.pma.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.jeipz.pma.entities.UserAccount;
+import com.jeipz.pma.services.UserAccountService;
+
+@Controller
+public class SecurityController {
+
+	@Autowired
+	UserAccountService userAccountService;
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptEncoder;
+	
+	@GetMapping("/register")
+	public String register(Model model) {
+		UserAccount userAccount = new UserAccount();
+		model.addAttribute("userAccount", userAccount);
+		return "security/register";
+	}
+
+	@PostMapping("/register/save")
+	public String saveUser(Model model, UserAccount user) {
+		user.setPassword(bCryptEncoder.encode(user.getPassword()));
+		userAccountService.save(user);
+		return "redirect:/login";
+	}
+	
+}
