c0478643a484ad98e0301cef75a85d674f85bc7b
rosariojp <rosario.jeipz@gmail.com>
2020-09-14 15:45:47 +0800

    Modify user account info retrieval and authorization

--- project-management/src/main/java/com/jeipz/pma/security/SecurityConfiguration.java
+++ project-management/src/main/java/com/jeipz/pma/security/SecurityConfiguration.java
@@ -1,47 +1,47 @@
 package com.jeipz.pma.security;
 
-import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	DataSource dataSource;
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptEncoder;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-		auth.inMemoryAuthentication()
-			.withUser("myuser")
-				.password("pass")
-				.roles("USER")
-			.and()
-			.withUser("user")
-				.password("pass2")
-				.roles("USER")
-			.and()
-			.withUser("manageruser")
-				.password("pass3")
-				.roles("ADMIN");
-		
-	}
-	
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		auth.jdbcAuthentication()
+			.usersByUsernameQuery("SELECT username, password, enabled " +
+				"FROM user_accounts WHERE username = ?")
+			.authoritiesByUsernameQuery("SELECT username, role " +
+				"FROM user_accounts WHERE username = ?")
+			.dataSource(dataSource)
+			.passwordEncoder(bCryptEncoder);
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.antMatchers("/projects/new").hasRole("ADMIN")
+			.antMatchers("/projects/save").hasRole("ADMIN")
 			.antMatchers("/employees/new").hasRole("ADMIN")
-			.antMatchers("/").authenticated().and().formLogin();
+			.antMatchers("/employees/save").hasRole("ADMIN")
+			.antMatchers("/", "/**").permitAll()
+			.and()
+			.formLogin();
 	}
 	
 }
