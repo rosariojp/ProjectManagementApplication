686f7178207b68684894244f202622141e8dcd49
rosariojp <rosario.jeipz@gmail.com>
2020-09-15 16:36:44 +0800

    Removed unused imports

--- project-management/src/main/java/com/jeipz/pma/controllers/EmployeeController.java
+++ project-management/src/main/java/com/jeipz/pma/controllers/EmployeeController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.jeipz.pma.dao.EmployeeRepository;
 import com.jeipz.pma.entities.Employee;
 import com.jeipz.pma.services.EmployeeService;
 
