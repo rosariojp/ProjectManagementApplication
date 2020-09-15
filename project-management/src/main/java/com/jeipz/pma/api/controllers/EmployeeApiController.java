547d4deb91ff7e2d462450310f946c80985088f5
rosariojp <rosario.jeipz@gmail.com>
2020-09-15 15:00:15 +0800

    Added Field Validation for Employee Entity

--- project-management/src/main/java/com/jeipz/pma/api/controllers/EmployeeApiController.java
+++ project-management/src/main/java/com/jeipz/pma/api/controllers/EmployeeApiController.java
@@ -1,5 +1,7 @@
 package com.jeipz.pma.api.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
@@ -36,18 +38,18 @@ public class EmployeeApiController {
 	
 	@PostMapping(consumes = "application/json")
 	@ResponseStatus(HttpStatus.CREATED)
-	public Employee create(@RequestBody Employee employee) {
+	public Employee create(@RequestBody @Valid Employee employee) {
 		return empRepo.save(employee); 
 	}
 	
 	@PutMapping(consumes = "application/json")
 	@ResponseStatus(HttpStatus.OK)
-	public Employee update(@RequestBody Employee employee) {
+	public Employee update(@RequestBody @Valid Employee employee) {
 		return empRepo.save(employee);
 	}
 	
 	@PatchMapping(path = "/{id}", consumes = "application/json")
-	public Employee partialUpdate(@PathVariable("id") long id, @RequestBody Employee patchEmployee) {
+	public Employee partialUpdate(@PathVariable("id") long id, @RequestBody @Valid Employee patchEmployee) {
 		Employee employee = empRepo.findById(id).get();
 		
 		if (patchEmployee.getEmail() != null) {
--- project-management/src/main/java/com/jeipz/pma/entities/Employee.java
+++ project-management/src/main/java/com/jeipz/pma/entities/Employee.java
@@ -3,6 +3,7 @@ package com.jeipz.pma.entities;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -12,8 +13,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.SequenceGenerator;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 public class Employee {
@@ -23,8 +28,16 @@ public class Employee {
 	@SequenceGenerator(name = "employee_seq", sequenceName = "employee_seq", allocationSize = 1)
 	private long employeeId;
 
+	@NotNull
 	private String firstName;
+	
+	@NotNull
+	@Size(min = 2, max = 50)
 	private String lastName;
+	
+	@NotNull
+	@Email
+	@Column(unique = true)
 	private String email;
 	
 	@ManyToMany(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.PERSIST },
