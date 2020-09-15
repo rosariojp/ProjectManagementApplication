419779e3106ebfe75a2b9d293f5958eccf7d7748
rosariojp <rosario.jeipz@gmail.com>
2020-09-15 16:18:06 +0800

    Apply pagination for employees and projects

--- project-management/src/main/java/com/jeipz/pma/api/controllers/EmployeeApiController.java
+++ project-management/src/main/java/com/jeipz/pma/api/controllers/EmployeeApiController.java
@@ -4,6 +4,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -73,6 +76,14 @@ public class EmployeeApiController {
 		} catch(EmptyResultDataAccessException e) {
 			
 		}
+	}
+	
+	@GetMapping(params = {"page", "size"})
+	@ResponseStatus(HttpStatus.OK)
+	public Iterable<Employee> findPaginatedEmployees(@RequestParam("page") int page,
+													 @RequestParam("size") int size) {
+		Pageable pageAndSize = PageRequest.of(page, size);
+		return empRepo.findAll(pageAndSize);
 	}
 	
 }
