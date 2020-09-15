ed675db4edfe2130a41ca61fd5c8fecd7b9def8f
rosariojp <rosario.jeipz@gmail.com>
2020-09-15 14:39:12 +0800

    Created Employee API REST Controller

---
+++ project-management/src/main/java/com/jeipz/pma/api/controllers/EmployeeApiController.java
@@ -0,0 +1,76 @@
+package com.jeipz.pma.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jeipz.pma.dao.EmployeeRepository;
+import com.jeipz.pma.entities.Employee;
+
+@RestController
+@RequestMapping("/app-api/employees")
+public class EmployeeApiController {
+	
+	@Autowired
+	EmployeeRepository empRepo;
+	
+	@GetMapping
+	public Iterable<Employee> getEmployees() {
+		return empRepo.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public Employee getEmployeeById(@PathVariable("id") long id) {
+		return empRepo.findById(id).get();
+	}
+	
+	@PostMapping(consumes = "application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Employee create(@RequestBody Employee employee) {
+		return empRepo.save(employee); 
+	}
+	
+	@PutMapping(consumes = "application/json")
+	@ResponseStatus(HttpStatus.OK)
+	public Employee update(@RequestBody Employee employee) {
+		return empRepo.save(employee);
+	}
+	
+	@PatchMapping(path = "/{id}", consumes = "application/json")
+	public Employee partialUpdate(@PathVariable("id") long id, @RequestBody Employee patchEmployee) {
+		Employee employee = empRepo.findById(id).get();
+		
+		if (patchEmployee.getEmail() != null) {
+			employee.setEmail(patchEmployee.getEmail());
+		}
+		if (patchEmployee.getFirstName() != null) {
+			employee.setFirstName(patchEmployee.getFirstName());
+		}
+		if (patchEmployee.getLastName() != null) {
+			employee.setLastName(patchEmployee.getLastName());
+		}
+		
+		return empRepo.save(employee);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable("id") long id) {
+		try {
+			empRepo.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			
+		}
+	}
+	
+}
